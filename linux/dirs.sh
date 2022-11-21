51158186dd503302b57bfcb0205f730eace717e5
Philippe MORISSEAU <philippe.morisseau@outlook.com>
2022-11-21 16:53:25 +0100

    fix: fast build

--- linux/dirs.sh
+++ linux/dirs.sh
@@ -9,20 +9,20 @@ ubuntu() {
   dirs() {
     BASE_DIR=$1
     echo "$BASE_DIR"
-    while read -r DOTNET_LINE_VERSION; do
-      DOTNET_VERSION=$(echo $DOTNET_LINE_VERSION | cut -d' ' -f1)
-      # fix unable install core-3.1 on ubuntu 22.04
-      if [[ "$BASE_DIR" != "ubuntu/22.04/azdo" || "$DOTNET_VERSION" != "core-3.1" ]]; then
-        echo "$BASE_DIR/dotnet/$DOTNET_VERSION"
-      fi
-    done < <(< derived/dotnet/versions sed '/^\s*#/d')
+    # while read -r DOTNET_LINE_VERSION; do
+    #   DOTNET_VERSION=$(echo $DOTNET_LINE_VERSION | cut -d' ' -f1)
+    #   # fix unable install core-3.1 on ubuntu 22.04
+    #   if [[ "$BASE_DIR" != "ubuntu/22.04/azdo" || "$DOTNET_VERSION" != "core-3.1" ]]; then
+    #     echo "$BASE_DIR/dotnet/$DOTNET_VERSION"
+    #   fi
+    # done < <(< derived/dotnet/versions sed '/^\s*#/d')
   }
 
   while read -r UBUNTU_VERSION; do
     echo "ubuntu/$UBUNTU_VERSION"
-    while read -r AZDO_AGENT_RELEASE; do
-      dirs "ubuntu/$UBUNTU_VERSION/azdo"
-    done < <(< versioned/releases sed '/^\s*#/d')
+    # while read -r AZDO_AGENT_RELEASE; do
+    #   dirs "ubuntu/$UBUNTU_VERSION/azdo"
+    # done < <(< versioned/releases sed '/^\s*#/d')
     while read -r AZDO_AGENT_RELEASE; do
       dirs "ubuntu/$UBUNTU_VERSION/aci-azdo"
     done < <(< versioned_for_aci/releases sed '/^\s*#/d')
